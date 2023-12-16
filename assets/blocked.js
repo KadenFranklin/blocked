db31566036df8a46e06171831249424f62eadc0b
Kaden Franklin <kadenfranklin@hotmail.com>
2023-12-15 20:15:02 -0600

    update blocked.js

--- assets/blocked.js
+++ assets/blocked.js
@@ -1,288 +1,164 @@
-// first thing should be a call to the twitter api to get the # of blocked accounts
-// store that in a variable and send it to _func()  intToString(integ){ }
 
-var digitsOld = [], digitsNew = [], list0 = ["0", "0", "0", "0"], list1 = ["3", "8", "9", "6"], list2 = ["0", "0", "0", "0"];
+var preset = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30],
+    list0 = ["0", "0", "0", "0"],
+    list1 = ["3", "8", "9", "6"],
+    list2 = ["0", "0", "0", "0"];
 
-makeUL(list0, "list0");
-makeUL(list0, "list1");
-makeUL(list2, "list2");
+createList("list0", list0);
+createList("list1", list1);
+createList("list2", list2);
 
-function intToList(n){
-  digits = String(n)
-  .split('')
-  .map(n);
-  return digits;
+function intToList(n) {
+    return String(n).split('').map(function (digit) {
+        return +digit;
+    });
 }
 
-function makeUL(array, varName) {
-  var listView = document.createElement("ul");
-  listView.id = varName;
-  var x = [];
-  if (array.length > 3) {
-    x = [3];
-  }
-  if (array.length > 6) {
-    x = [3, 6];
-  }
-  if (array.length > 9) {
-    x = [3, 6, 9];
-  }
-  if (array.length > 12) {
-    x = [3, 6, 9, 12];
-  }
-  if (array.length > 15) {
-    x = [3, 6, 9, 12, 15];
-  }
-  for (var i = 0; i < array.length; i++) {
-    for (var j = 0; j < x.length; j++) {
-      if (i == x[j]) {
-        var listViewItem = document.createElement("li");
-        listViewItem.className = "seperator";
-        if (listView.id != "list1") {
-          listViewItem.style.opacity = 0;
-        } else {
-          listViewItem.style.opacity = 1;
+function createList(elementId, items) {
+    var listView = document.createElement("ul");
+    listView.id = elementId;
+
+    items.forEach(function (item, index) {
+        if (preset.includes(index)) {
+            var separator = createListItem(',', index, true, elementId == "list1");
+            separator.className = "separator";
+            separator.opacity = 1;
+            listView.appendChild(separator);
         }
-        // set position x and y
-        listViewItem.appendChild(document.createTextNode(","));
+        var listViewItem = createListItem(item, index, false, elementId == "list1");
         listView.appendChild(listViewItem);
-      }
-    }
-    var listViewItem = document.createElement("li");
-    listViewItem.className = "";
-    if (listView.id != "list1") {
-      listViewItem.style.opacity = 0;
-    } else {
-      listViewItem.style.opacity = 1;
-    }
-    // set position x and y
-    listViewItem.appendChild(document.createTextNode(array[i]));
-    listView.appendChild(listViewItem);
-  }
-  return document.getElementById("wrap").appendChild(listView);
-}
+    });
 
-function checkLists(x, y) {
-  for (var i = 0, c = x.length; i < c; i++) {
-    if (x[i] != y[i]) {
-      animate(i, x[i], y[i]);
+    var listContainer = document.getElementById("wrap");
+    var existingList = document.getElementById(elementId);
+    if (existingList) {
+        existingList.replaceWith(listView);
+    } else {
+        listContainer.appendChild(listView);
     }
-  }
+}
+function createListItem(content, index, isSeparator, transparent) {
+    var listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(content));
+    if (transparent) {
+        listItem.style.opacity = 1;
+    }
+    else {
+        listItem.style.opacity = 0;
+    }
+    return listItem;
+}
+function checkLists(oldList, newList) {
+    oldList.forEach(function (oldItem, index) {
+        var newItem = newList[index];
+        if (oldItem !== newItem) {
+            animate(index, oldItem, newItem);
+        }
+    });
 }
 
 function animate(id, digitOld, digitNew) {
-  var sumChange = digitNew - digitOld;
-  if (sumChange <= 0) {
-    sumChange = digitOld - digitNew;
-  }
-  if (id >= 15) {
-    id += 1;
-  }
-  if (id >= 12) {
-    id += 1;
-  }
-  if (id >= 9) {
-    id += 1;
-  }
-  if (id >= 6) {
-    id += 1;
-  }
-  if (id >= 3) {
-    id += 1;
-  }
-  if (digitOld.length == digitNew.length) {
-    for (i = 0; i < sumChange; i++) {
-      if ( document.getElementById("list2 li:nth-child(" + id + ")").innerHTML ==",") {
-        document.getElementById("list2").children[id].innerHTML = digitNew;
-      }
-      if ( document.getElementById("list1").children[id].innerHTML != digitOld) {
-        document.getElementById("list1").children[id].innerHTML = digitOld;
-      }
+    var sumChange = digitNew - digitOld;
+    if (sumChange <= 0) {
+        sumChange = digitOld - digitNew;
     }
-    if (digitNew > digitOld) {
-      doMove(id, "list1", "list2");
+    if (id >= 15) {
+        id += 1;
     }
-    if (digitOld > digitNew) {
-      moveDown(id, "list1", "list0");
+    if (id >= 12) {
+        id += 1;
     }
-  }
+    if (id >= 9) {
+        id += 1;
+    }
+    if (id >= 6) {
+        id += 1;
+    }
+    if (id >= 3) {
+        id += 1;
+    }
+    if (digitOld.length == digitNew.length) {
+        for (i = 0; i < sumChange; i++) {
+            if (document.getElementById("list1").children[id].innerHTML != digitOld) {
+                document.getElementById("list1").children[id].innerHTML = digitOld;
+            }
+        }
+        if (digitNew > digitOld) {
+            moveUp(id, "list1", "list2");
+        }
+        //if (digitOld > digitNew) {
+        //    moveDown(id, "list1", "list0");
+        //}
+    }
 }
 
-function reset(id, name1, name2){
-  var val1 =   document.getElementById(name1).children[id].innerHTML;
-  val2 = document.getElementById(name2).children[id].innerHTML;
-  document.getElementById(name1).children[id].innerHTML = val2;
-  document.getElementById(name2).children[id].innerHTML = val1;  
-  document.getElementById(name1).children[id].style.top = 0 +'px';
-  document.getElementById(name2).children[id].style.top = 0 +'px';
-  document.getElementById("list0").children[id].style.opacity = 0;
-  document.getElementById("list1").children[id].style.opacity = 1;
-  document.getElementById("list2").children[id].style.opacity = 0;
-}
-
-function doMove(id, name1, name2) {
-  document.getElementById(name2).children[id].style.opacity = 0.01;
-  let pos = 0;
-  idInt = setInterval(frame, 20);
-  function frame() {
-    if (pos == 100) {
-      reset(id, name1, name2);
-      clearInterval(idInt);
-    } else {
-      pos++;
-      opac2 = parseFloat(document.getElementById(name2).children[id].style.opacity);
-      document.getElementById(name1).children[id].style.opacity -= 1/100;
-      document.getElementById(name2).children[id].style.opacity = opac2 + 0.01;
-      document.getElementById(name1).children[id].style.top = -pos + "px";
-      document.getElementById(name2).children[id].style.top = -pos + "px";
+function moveUp(id, name1, name2) {
+    document.getElementById(name2).children[id].style.opacity = 0.01;
+    let pos = 0;
+    idInt = setInterval(frame, 25);
+    function frame() {
+        if (pos == 100) {
+            clearInterval(idInt);
+        } else {
+            pos++;
+            opac2 = parseFloat(document.getElementById(name2).children[id].style.opacity);
+            document.getElementById(name1).children[id].style.opacity -= 1 / 100;
+            document.getElementById(name2).children[id].style.opacity = opac2 + 1/100;
+            document.getElementById(name1).children[id].style.top = -pos + "px";
+            document.getElementById(name2).children[id].style.top = -pos + "px";
+        }
     }
-  }
 }
 
 function moveDown(id, name1, name2) {
-  document.getElementById(name2).children[id].style.opacity = 0.1;
-  let pos = 0;
-  let idInt = setInterval(frame, 20);
-  function frame() {
-    if (pos == 100) {
-      reset(id, name1, name2);
-      clearInterval(idInt);
-    } else {
-      pos ++;
-      opac2 = parseFloat(document.getElementById(name2).children[id].style.opacity);
-      document.getElementById(name1).children[id].style.opacity -= 1/100;
-      document.getElementById(name2).children[id].style.opacity = opac2 + 0.01;
-      document.getElementById(name1).children[id].style.top = pos + "px";
-      document.getElementById(name2).children[id].style.top = pos + "px";
+    document.getElementById(name2).children[id].style.opacity = 0.1;
+    let pos = 0;
+    let idInt = setInterval(frame, 25);
+    function frame() {
+        if (pos == 100) {
+            clearInterval(idInt);
+        } else {
+            pos++;
+            opac2 = parseFloat(document.getElementById(name2).children[id].style.opacity);
+            document.getElementById(name1).children[id].style.opacity -= 1 / 100;
+            document.getElementById(name2).children[id].style.opacity = opac2 + 1/100;
+            document.getElementById(name1).children[id].style.top = pos + "px";
+            document.getElementById(name2).children[id].style.top = pos + "px";
+        }
     }
-  }
 }
 
-function replaceUL(a0, a1, a2){
-  if (a0.length > 3) {
-    x0 = [3];
-  }
-  if (a0.length > 6) {
-    x0 = [3, 6];
-  }
-  if (a0.length > 9) {
-    x0 = [3, 6, 9];
-  }
-  if (a0.length > 12) {
-    x0 = [3, 6, 9, 12];
-  }
-  if (a0.length > 15) {
-    x0 = [3, 6, 9, 12, 15];
-  }
-  if (document.getElementById("list0")){
-    var newElement = document.createElement("ul");
-    for (var i = 0; i < a0.length; i++) {
-      for (var j = 0; j < x0.length; j++) {
-        if (i == x0[j]) {
-          var listViewItem = document.createElement("li");
-          listViewItem.className = "seperator";
-          listViewItem.style.opacity = 0;
-          // set position, x and y
-          listViewItem.appendChild(document.createTextNode(","));
-          newElement.appendChild(listViewItem);
+function updateLists(listIds, itemArrays) {
+    listIds.forEach(function (listId, index) {
+        var listElement = document.getElementById(listId);
+        if (listElement) {
+            listElement.parentNode.removeChild(listElement);
         }
-      }
-      var listViewItem = document.createElement("li");
-      listViewItem.className = "";
-      listViewItem.style.opacity = 0;
-      // set position, x and y
-      listViewItem.appendChild(document.createTextNode(a0[i]));
-      newElement.appendChild(listViewItem);
-    }
-    newElement.id = "list0";
-    var lis0 = document.getElementById('list0');
-    lis0.replaceWith(newElement);
-  }
-  if (a1.length > 3) {
-    x1 = [3];
-  }
-  if (a1.length > 6) {
-    x1 = [3, 6];
-  }
-  if (a1.length > 9) {
-    x1 = [3, 6, 9];
-  }
-  if (a1.length > 12) {
-    x1 = [3, 6, 9, 12];
-  }
-  if (a1.length > 15) {
-    x1 = [3, 6, 9, 12, 15];
-  }
-  if (document.getElementById("list1")){
-    var newElement = document.createElement("ul");
-    for (var i = 0; i < a1.length; i++) {
-      for (var j = 0; j < x1.length; j++) {
-        if (i == x1[j]) {
-          var listViewItem = document.createElement("li");
-          listViewItem.className = "seperator";
-          listViewItem.style.opacity = 1;
-          // set position, x and y
-          listViewItem.appendChild(document.createTextNode(","));
-          newElement.appendChild(listViewItem);
+        createList(listId, itemArrays[index]);
+    });
+
+    compareAndAnimate(itemArrays);
+}
+function compareAndAnimate(itemArrays) {
+    var maxLength = Math.max(itemArrays[0].length, itemArrays[1].length, itemArrays[2].length);
+    for (var i = 0; i < maxLength; i++) {
+        var currentItem = itemArrays[0][i],
+            nextItem = itemArrays[1][i];
+        if (currentItem !== nextItem) {
+            checkLists(itemArrays[0], itemArrays[1]); // comparing list0 and list1
+            break;
         }
-      }
-      var listViewItem = document.createElement("li");
-      listViewItem.className = "";
-      listViewItem.style.opacity = 1;
-      // set position, x and y
-      listViewItem.appendChild(document.createTextNode(a1[i]));
-      newElement.appendChild(listViewItem);
     }
-    newElement.id = "list1";
-    var lis1 = document.getElementById('list1');
-    lis1.replaceWith(newElement);
-  }
-  if (a2.length > 3) {
-    x2 = [3];
-  }
-  if (a2.length > 6) {
-    x2 = [3, 6];
-  }
-  if (a2.length > 9) {
-    x2 = [3, 6, 9];
-  }
-  if (a2.length > 12) {
-    x2 = [3, 6, 9, 12];
-  }
-  if (a2.length > 15) {
-    x2 = [3, 6, 9, 12, 15];
-  }
-  if (document.getElementById("list2")){
-    var newElement = document.createElement("ul");
-    for (var i = 0; i < a2.length; i++) {
-      for (var j = 0; j < x2.length; j++) {
-        if (i == x2[j]) {
-          var listViewItem = document.createElement("li");
-          listViewItem.className = "seperator";
-          listViewItem.style.opacity = 0;
-          // set position, x and y
-          listViewItem.appendChild(document.createTextNode(","));
-          newElement.appendChild(listViewItem);
+
+    for (var i = 0; i < maxLength; i++) {
+        var currentItem = itemArrays[1][i],
+            nextItem = itemArrays[2][i];
+        if (currentItem !== nextItem) {
+            checkLists(itemArrays[1], itemArrays[2]); // comparing list1 and list2
+            break;
         }
-      }
-      var listViewItem = document.createElement("li");
-      listViewItem.className = "";
-      listViewItem.style.opacity = 0;
-      // set position, x and y
-      listViewItem.appendChild(document.createTextNode(a2[i]));
-      newElement.appendChild(listViewItem);
     }
-    newElement.id = "list2";
-    var lis2 = document.getElementById('list2');
-    lis2.replaceWith(newElement);
-  }
-  if ( a0[a0.length - 1] > a1[a1.length - 1]){
-    checkLists(a0, a1);
-  }
-  if ( a1[a1.length - 1] < a2[a2.length - 1]){
-    checkLists(a1, a2);
-  }
 }
 
-// setTimeout('', 3000);
-replaceUL(list0, list1, list2);
+updateLists(["list0", "list1", "list2"], [list0, list0, list1]);
+
